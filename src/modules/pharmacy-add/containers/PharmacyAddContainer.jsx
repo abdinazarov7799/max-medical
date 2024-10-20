cfcce77401785ba9f82a27331b41458acadfea3f
abdinazarov7799 <parolxato@gmail.com>
2024-10-20 23:49:04 +0500

    feat: pharmacy add form products

--- src/modules/pharmacy-add/containers/PharmacyAddContainer.jsx
+++ src/modules/pharmacy-add/containers/PharmacyAddContainer.jsx
@@ -3,34 +3,39 @@ import Container from "../../../components/Container.jsx";
 import usePostQuery from "../../../hooks/api/usePostQuery.js";
 import {URLS} from "../../../constants/url.js";
 import {useTelegram} from "../../../hooks/useTelegram.jsx";
-import {Button, Form, Input, InputNumber, Space, Typography} from "antd";
+import {Button, Col, Form, Input, InputNumber, Row, Space, Spin, Typography} from "antd";
 import {get} from "lodash";
+import useGetAllQuery from "../../../hooks/api/useGetAllQuery.js";
+import {KEYS} from "../../../constants/key.js";
 const { Title } = Typography;
 
 const PharmacyAddContainer = ({userId}) => {
-    const { mutate, isLoading } = usePostQuery({});
+    const { mutate, isLoading:isLoadingPost } = usePostQuery({});
     const { onClose } = useTelegram();
     const [form] = Form.useForm();
+    const {data,isLoading} = useGetAllQuery({
+        url: URLS.medicine_list,
+        key: KEYS.medicine_list,
+    })
 
     const onFinish = (values) => {
+        const remainCount = {};
+        const reservationCount = {};
+
+        get(data, 'data', [])?.forEach(product => {
+            const productId = get(product, 'id');
+            remainCount[productId] = get(values, `remainCount-${productId}`, 0);
+            reservationCount[productId] = get(values, `reservationCount-${productId}`, 0);
+        });
+
         const formData = {
             chatId: userId,
             address: get(values,'address'),
             name: get(values,'name'),
             pharmacistFIO: get(values,'pharmacistFIO'),
             phoneNumber: get(values,'phoneNumber'),
-            remainCount: {
-                1: get(values,'remainCount-1',0),
-                2: get(values,'remainCount-2',0),
-                3: get(values,'remainCount-3',0),
-                4: get(values,'remainCount-4',0),
-            },
-            reservationCount: {
-                1: get(values,'reservationCount-1',0),
-                2: get(values,'reservationCount-2',0),
-                3: get(values,'reservationCount-3',0),
-                4: get(values,'reservationCount-4',0),
-            },
+            remainCount,
+            reservationCount,
         }
         mutate(
             { url: URLS.pharmacy_add, attributes: formData },
@@ -83,64 +88,44 @@ const PharmacyAddContainer = ({userId}) => {
                         <Input />
                     </Form.Item>
                     <Title level={4}>Остатка</Title>
-                    <Space>
-                        <Form.Item
-                            label={"Елекса"}
-                            name="remainCount-1">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label={"Л-Аргенин"}
-                            name="remainCount-2">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-                    </Space>
-
-                    <Space>
-                        <Form.Item
-                            label={"Кардон"}
-                            name="remainCount-3">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label={"Фертуна"}
-                            name="remainCount-4">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-                    </Space>
+                    <Spin spinning={isLoading} tip={"Юкланмоқда..."}>
+                        <Row justify={"space-between"} align={"middle"} gutter={[10,10]}>
+                            {
+                                get(data,'data',[])?.map(product => {
+                                    return (
+                                        <Col span={12} key={get(product,'id')}>
+                                            <Form.Item
+                                                label={get(product,'name')}
+                                                name={`remainCount-${get(product,'id')}`}>
+                                                <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
+                                            </Form.Item>
+                                        </Col>
+                                    )
+                                })
+                            }
+                        </Row>
+                    </Spin>
                     <Title level={4}>Бронь</Title>
-                    <Space>
-                        <Form.Item
-                            label={"Елекса"}
-                            name="reservationCount-1">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label={"Л-Аргенин"}
-                            name="reservationCount-2">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-                    </Space>
-
-                    <Space>
-                        <Form.Item
-                            label={"Кардон"}
-                            name="reservationCount-3">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-
-                        <Form.Item
-                            label={"Фертуна"}
-                            name="reservationCount-4">
-                            <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
-                        </Form.Item>
-                    </Space>
+                    <Spin spinning={isLoading} tip={"Юкланмоқда..."}>
+                        <Row justify={"space-between"} align={"middle"} gutter={[10,10]}>
+                            {
+                                get(data,'data',[])?.map(product => {
+                                    return (
+                                        <Col span={12} key={get(product,'id')}>
+                                            <Form.Item
+                                                label={get(product,'name')}
+                                                name={`reservationCount-${get(product,'id')}`}>
+                                                <InputNumber min={0} defaultValue={0} style={{width: "100%"}}/>
+                                            </Form.Item>
+                                        </Col>
+                                    )
+                                })
+                            }
+                        </Row>
+                    </Spin>
 
                     <Form.Item>
-                        <Button block type="primary" htmlType="submit" loading={isLoading}>
+                        <Button block type="primary" htmlType="submit" loading={isLoadingPost}>
                             Юбориш
                         </Button>
                     </Form.Item>
